84951f6e959b52fea64564e0ebdfe2f485ee2ade
Diogo Bernini Milagres <dmilagres@ltu.edu>
2014-05-29 17:16:57 -0400

    Window to edit field created

--- Workspace/visimob desktop/visimob desktop/visimob desktop/Imovel.xaml.cs
+++ Workspace/visimob desktop/visimob desktop/visimob desktop/Imovel.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +20,21 @@ namespace visimob_desktop
     /// </summary>
     public partial class Imovel : Window
     {
+        SQLiteDatabase db;
+        string url2, index;
+        DataTable info;
         public Imovel(string url, string index)
         {
+            db = new SQLiteDatabase(url);
+            url2 = url;
+            info = db.GetDataTable("SELECT * FROM Imoveis");
             InitializeComponent();
             this.WindowState = WindowState.Maximized;
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
